9a821d52820e719bf64ec33d74088b4987b46674
BernardoModerno <bernardo.moderno@gmail.com>
2023-02-06 14:16:25 -0300

    Setting Up Themes

--- amazon_clone_tutorial/lib/main.dart
+++ amazon_clone_tutorial/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:amazon_clone_tutorial/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,16 +13,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Amazon Clone',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+            elevation: 0,
+            iconTheme: IconThemeData(
+              color: Colors.black,
+            )),
       ),
       home: Scaffold(
         appBar: AppBar(
           title: Center(child: const Text('Hello')),
         ),
-        body: const Center(
-          child: Text(
-            'Flutter Demo Home Page',
-          ),
+        body: Column(
+          children: [
+            const Center(
+              child: Text(
+                'Flutter Demo Home Page',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Click'),
+            ),
+          ],
         ),
       ),
     );
